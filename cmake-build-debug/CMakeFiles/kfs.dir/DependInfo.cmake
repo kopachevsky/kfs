a4857e077bd0b86b822e5652b355999cbac0e0ad
klevchenko <sigmund313@gmail.com>
2019-07-05 14:39:30 +0300

    update

--- cmake-build-debug/CMakeFiles/kfs.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/kfs.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/home/donelkostino/bricks/kfs/main.cpp" "/home/donelkostino/bricks/kfs/cmake-build-debug/CMakeFiles/kfs.dir/main.cpp.o"
+  "/home/donelkostino/kfs/main.cpp" "/home/donelkostino/kfs/cmake-build-debug/CMakeFiles/kfs.dir/main.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/donelkostino/bricks/kfs
+# Install script for directory: /home/donelkostino/kfs
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/donelkostino/bricks/kfs/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/donelkostino/kfs/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
