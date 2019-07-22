d6709bd03242ac9e3c6073355eb3701fb705f32c
klevchenko <sigmund313@gmail.com>
2019-07-22 16:15:10 +0300

    fix

--- tests/kfs_mkdir_test.c
+++ tests/kfs_mkdir_test.c
@@ -15,13 +15,10 @@ void kfs_mkdir_teardown(void) {
     main_teardown();
 }
 
-START_TEST(kfs_mkdir_base) {
+START_TEST(kfs_mkdir_creation) {
     int res = kfs_mkdir("mydir", 0777);
-    ck_assert_int_eq(0, res);
-
-    printf("%s\n", LOCAL_DISC_CACHE_PATH);
-    char *path = str_concat(LOCAL_DISC_CACHE_PATH, "mydir");
-    // TODO at this point we need to assert that directory with correct permissions is created
+    char *dir_path = str_concat(MOUNT_PATH, "mydir");
+    ck_assert_str_eq("/ext/data1/mydir", dir_path);
 }
 END_TEST
 
@@ -29,7 +26,7 @@ Suite * kfs_mkdir_suite(void) {
     Suite *suite = suite_create("kfs_mkdir()");
     TCase *tcase = tcase_create("Test Cases with Setup and Teardown");
     tcase_add_checked_fixture(tcase, kfs_mkdir_setup, kfs_mkdir_teardown);
-    tcase_add_test(tcase, kfs_mkdir_base);
+    tcase_add_test(tcase, kfs_mkdir_creation);
     suite_add_tcase(suite, tcase);
     return suite;
 }
