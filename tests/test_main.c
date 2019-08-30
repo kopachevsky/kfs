78e3a356661d8e6083e8f206f5f669ed2ed0505f
klevchenko <sigmund313@gmail.com>
2019-08-30 14:14:22 +0300

    kfs_releasedir unit test fix

--- tests/test_main.c
+++ tests/test_main.c
@@ -44,7 +44,7 @@ int main(void) {
     srunner_add_suite(sr, kfs_readlink_suite());
     srunner_add_suite(sr, kfs_fsync_suite());
     srunner_add_suite(sr, kfs_opendir_suite());
-//    srunner_add_suite(sr, kfs_releasedir_suite());
+    srunner_add_suite(sr, kfs_releasedir_suite());
 //    srunner_add_suite(sr, kfs_mknod_suite());
 //    srunner_add_suite(sr, kfs_readdir_suite());
     srunner_set_fork_status(sr, CK_NOFORK);
