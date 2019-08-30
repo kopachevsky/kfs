16613d286686dbbcc29b75ab9643bfb6ec115a33
klevchenko <sigmund313@gmail.com>
2019-08-30 14:30:51 +0300

    kfs_readdir unit test fix

--- tests/test_main.c
+++ tests/test_main.c
@@ -46,7 +46,7 @@ int main(void) {
     srunner_add_suite(sr, kfs_opendir_suite());
     srunner_add_suite(sr, kfs_releasedir_suite());
     srunner_add_suite(sr, kfs_mknod_suite());
-//    srunner_add_suite(sr, kfs_readdir_suite());
+    srunner_add_suite(sr, kfs_readdir_suite());
     srunner_set_fork_status(sr, CK_NOFORK);
     srunner_set_log (sr, "test.log");
     srunner_set_xml (sr, "test.xml");
