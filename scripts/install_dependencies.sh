cf9ee4fb2a3ea37dbadbd53cd797c6f18d673403
klevchenko <sigmund313@gmail.com>
2019-09-21 12:29:57 +0300

    dependencies update

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,8 +1,2 @@
 apt-get update
-apt-get install -y check
-apt-get install -y cmake
-apt-get install -y bison
-apt-get install -y glusterfs-client
-apt-get install -y pkg-config uuid-dev
-apt-get install -y libfuse-dev
-apt-get install -y zlib1g-dev
+apt-get install -y check cmake bison glusterfs-client pkg-config uuid-dev libfuse-dev zlib1g-dev
