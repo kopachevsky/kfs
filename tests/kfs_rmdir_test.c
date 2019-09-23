50ac737d8eb8df290d9b9f74fbef29a7d85c7e51
klevchenko <sigmund313@gmail.com>
2019-09-23 08:49:23 +0300

    fixed kfs_rmdir unit test

--- tests/kfs_rmdir_test.c
+++ tests/kfs_rmdir_test.c
@@ -14,13 +14,11 @@ void kfs_rmdir_teardown(void) {
 }
 
 START_TEST(kfs_rmdir_base) {
-    int res = kfs_mkdir("rmdir_base/", 0777);
+    char *path = "kfs_rmdir_base/";
+    int res = kfs_mkdir(path, 777);
     ck_assert_int_eq(res, 0);
-    res = kfs_rmdir("rmdir_base/");
-    ck_assert_int_eq(res, 0);
-    DIR* dir = opendir("rmdir_base/");
-    fail_if(dir != NULL);
-    rmdir("/tmp/CACHE/rmdir_base/");
+    res = kfs_rmdir(path);
+    ck_assert_int_eq(res,0);
 }
 END_TEST
 
