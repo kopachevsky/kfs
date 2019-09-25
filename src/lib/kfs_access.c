95f0d3f221ee4e503219f4be12129e40f1c21457
klevchenko <sigmund313@gmail.com>
2019-09-25 16:51:53 +0300

    added glusterfs_api access function to kfs_access

--- src/lib/kfs_access.c
+++ src/lib/kfs_access.c
@@ -7,5 +7,9 @@ int kfs_access(const char* path, int mask) {
     if (res == -1) {
         return -errno;
     }
+    res = xglfs_access(path, mask);
+    if (res == -1) {
+        return -errno;
+    }
     return 0;
 }
