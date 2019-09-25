a79a812446cb7f1c07e7d6dfe07bd1fba82409ed
klevchenko <sigmund313@gmail.com>
2019-09-25 16:53:24 +0300

    added glusterfs_api link function to kfs_link

--- src/lib/kfs_link.c
+++ src/lib/kfs_link.c
@@ -9,5 +9,9 @@ int kfs_link(const char* source_path, const char* target_path) {
     if (res == -1) {
         return -errno;
     }
+    res = xglfs_link(source_path, target_path);
+    if (res == -1) {
+        return -errno;
+    }
     return 0;
 }
