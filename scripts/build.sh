d5eeb6338ad02f722a713494213d02d0513d09de
klevchenko <sigmund313@gmail.com>
2019-09-27 12:09:10 +0300

    build script update

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,4 @@
 mkdir build && cd build
 cmake .. && make
 cd -
-cd /tmp && mkdir kfs && cd kfs && mkdir CACHE && mkdir VOLUME && mkdir MOUNTPOINT && cd -
+cd /tmp && mkdir kfs && cd kfs && mkdir CACHE && mkdir MOUNTPOINT && cd -
