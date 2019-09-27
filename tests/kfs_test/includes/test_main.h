8ee96f926c0f4ab76da6c4db34b97d32fd35bdd5
klevchenko <sigmund313@gmail.com>
2019-09-27 12:11:39 +0300

    test directory refactoring

--- tests/kfs_test/includes/test_main.h
+++ tests/kfs_test/includes/test_main.h
@@ -1,3 +1,9 @@
+#include <check.h>
+#include <stdlib.h>
+#include <kfs_common.h>
+
+#include "kfs_suites.h"
+
 void main_setup(void);
 void main_teardown(void);
 struct fuse_file_info init_struct(int flag);
