cf595e7bbaf01f01a71e321ee596ea7369324d2d
klevchenko <sigmund313@gmail.com>
2019-10-01 14:55:03 +0300

    fixed segmentation fault while working with fs

--- src/lib/kfs_opendir.c
+++ src/lib/kfs_opendir.c
@@ -7,6 +7,12 @@ int kfs_opendir(const char* path, struct fuse_file_info *fi) {
     if (dp == NULL) {
         return -errno;
     }
+    if (XGLFS_STATE->gluster_api) {
+        int res = xglfs_opendir(path, fi);
+        if (res == -1) {
+            return -errno;
+        }
+    }
     fi->fh = (intptr_t) dp;
     return 0;
 }
