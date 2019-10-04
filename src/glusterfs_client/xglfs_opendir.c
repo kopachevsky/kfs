b37bbe7f1e05e3ae001e6c62d4859dfc4cfc2c78
klevchenko <sigmund313@gmail.com>
2019-10-04 11:33:45 +0300

    added debug massages

--- src/glusterfs_client/xglfs_opendir.c
+++ src/glusterfs_client/xglfs_opendir.c
@@ -1,13 +1,14 @@
 #include "xglfs_opendir.h"
 
 int xglfs_opendir(const char* _path, struct fuse_file_info* _info) {
-    int res = 0;
+//    int res = 0;
     glfs_fd_t* fd = glfs_opendir(XGLFS_STATE->fs, _path);
     if (unlikely(!fd)) {
-        res = -errno;
+        return -errno;
     }
-    if (likely(res == 0)) {
-        _info->fh = FD_TO_FH(fd);
-    }
-    return res;
+    _info->fh = FD_TO_FH(fd);
+//    if (likely(res == 0)) {
+//        _info->fh = FD_TO_FH(fd);
+//    }
+    return 0;
 }
--- src/lib/kfs_opendir.c
+++ src/lib/kfs_opendir.c
@@ -4,16 +4,20 @@ int kfs_opendir(const char* path, struct fuse_file_info *fi) {
     char fpath[PATH_MAX];
     fullpath(fpath, path);
     DIR *dp = opendir(fpath);
+    fprintf(stderr,"%s\n", "kfs opendir");
+    fprintf(stderr, "%d\n", -errno);
     if (dp == NULL) {
         return -errno;
     }
+    fi->fh = (intptr_t) dp;
     if (XGLFS_STATE->gluster_api) {
         int res = xglfs_opendir(path, fi);
+        fprintf(stderr,"%s\n", "xglfs opendir");
+        fprintf(stderr, "%d\n", -errno);
         if (res == -1) {
             return -errno;
         }
     }
-    fi->fh = (intptr_t) dp;
     return 0;
 }
 
