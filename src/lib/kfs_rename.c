0d2dd9e3790660dfcb1d2d729a02c426af157b4f
KostiaLevchenko <sigmund313@gmail.com>
2019-10-09 21:36:41 +0300

    added xglfs_rename function

--- src/lib/kfs_rename.c
+++ src/lib/kfs_rename.c
@@ -9,5 +9,11 @@ int kfs_rename(const char* source_path, const char* target_path) {
     if (res == -1) {
         return -errno;
     }
+    if (XGLFS_STATE->gluster_api) {
+        int ret = xglfs_rename(source_path, target_path);
+        if (ret == -1) {
+            return -errno;
+        }
+    }
     return 0;
 }
