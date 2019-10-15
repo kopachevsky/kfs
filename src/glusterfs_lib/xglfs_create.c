fef6437e34206d69631da60699565fa2625cbcc3
KostiaLevchenko <sigmund313@gmail.com>
2019-10-15 16:43:45 +0300

    added create flags

--- src/glusterfs_lib/xglfs_create.c
+++ src/glusterfs_lib/xglfs_create.c
@@ -1,9 +1,7 @@
 #include "xglfs_create.h"
 
 int xglfs_create(const char *path, mode_t mode, struct fuse_file_info *fi) {
-    (void)fi;
-    glfs_fd_t* fd = glfs_creat(XGLFS_STATE->fs, path, O_CREAT | O_WRONLY | O_TRUNC, mode);
-    printf("xglfs_create execute result : %lu\n", FD_TO_FH(fd));
+    glfs_fd_t* fd = glfs_creat(XGLFS_STATE->fs, path, fi->flags, mode);
     if (unlikely(!fd)) {
         return -errno;
     }
