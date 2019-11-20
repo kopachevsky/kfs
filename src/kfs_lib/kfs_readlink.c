5e74a7369ab6bc218decfe1cfd7b62ef6386c5dc
KostiaLevchenko <sigmund313@gmail.com>
2019-11-20 15:16:10 +0200

    fixed fuse bad value

--- src/kfs_lib/kfs_readlink.c
+++ src/kfs_lib/kfs_readlink.c
@@ -4,11 +4,10 @@ int kfs_readlink(const char *path, char *buf, size_t size) {
     log_debugf("kfs_readlink start  %s\n", path);
     char fpath[PATH_MAX_EXTENDED] = {0};
     fullpath(fpath, path);
-    int res = 0;
     log_debugf("    kfs_readlink fullpath : %s\n", fpath);
     set_current_user();
     fuse_context_log();
-    res = readlink(fpath, buf, size-1);
+    int res = readlink(fpath, buf, size-1);
     set_default_user();
     if (res == -1) {
         log_errorf("Error kfs_readlink : %s\n", strerror( errno ));
@@ -16,5 +15,5 @@ int kfs_readlink(const char *path, char *buf, size_t size) {
     }
     buf[res] = '\0';
     log_debugf("kfs_readlink exit result : %d\n", res);
-    return res;
+    return 0;
 }
