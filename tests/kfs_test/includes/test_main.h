029cc37e6486ca79651301cdf5258a08ac07229a
KostiaLevchenko <sigmund313@gmail.com>
2019-11-27 16:22:30 +0200

    removed old unit tests

--- tests/kfs_test/includes/test_main.h
+++
@@ -1,9 +0,0 @@
-#include <check.h>
-#include <stdlib.h>
-#include <kfs_common.h>
-
-#include "kfs_suites.h"
-
-void main_setup(void);
-void main_teardown(void);
-struct fuse_file_info init_struct(int flag);
